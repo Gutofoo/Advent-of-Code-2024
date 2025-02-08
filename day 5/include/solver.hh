f1cb15323e3acd7d1061397d40c291af63bba25c
Augusto Dantas de Oliveira <guto@de-oliveira.org>
2025-02-07 22:54:54 -0800

    mild refactor

--- day 5/include/solver.hh
+++ day 5/include/solver.hh
@@ -25,6 +25,8 @@ class Solver {
 
         std::vector<rule> ExtractRelevantRules(const update& current_update);
 
+        bool CheckUpdateValidity(const update& current_update, const std::vector<rule>& relevant_rules);
+
 };
 
 template<parts p>
@@ -45,6 +47,37 @@ std::vector<rule> Solver<p>::ExtractRelevantRules(const update& current_update) 
 
 
     return relevant_rules;
+
+}
+
+template<parts p>
+bool Solver<p>::CheckUpdateValidity(const update& current_update, const std::vector<rule>& relevant_rules) {
+
+    // track printed pages while assessing the current page in the requested update
+    std::vector<page> printed_pages;
+    for(size_t j = 0; j < current_update.size(); j++) {
+
+        // extract the next requested page in the update
+        const page& current_page = current_update[j];
+
+        // go through the relevant rules
+        for(const rule current_rule : relevant_rules) {
+
+            // if this page appears in the second part of a rule, must check that all prerequisites are met
+            if(current_page == current_rule.second) {
+                if(std::none_of(printed_pages.begin(), printed_pages.end(), [current_rule](const page& printed_page){ return printed_page == current_rule.first; })) {
+                    // failed check: prerequisite has not been met!
+                    // skip to next update
+                    return false;
+                }
+            }
+        }
+
+        printed_pages.push_back(current_page);
+
+    }
+
+    return true;
 
 }
 
--- day 5/src/solver.cpp
+++ day 5/src/solver.cpp
@@ -23,39 +23,17 @@ int Solver<parts::PART1>::solve() {
         // let's extract the relevant rules for this update
         std::vector<rule> relevant_rules = ExtractRelevantRules(current_update);
 
-        // track printed pages while assessing the current page in the requested update
-        std::vector<page> printed_pages;
-        for(size_t j = 0; j < current_update.size(); j++) {
-
-            // extract the next requested page in the update
-            const page& current_page = current_update[j];
-
-            // go through the relevant rules
-            for(const rule current_rule : relevant_rules) {
-
-                // if this page appears in the second part of a rule, must check that all prerequisites are met
-                if(current_page == current_rule.second) {
-                    if(std::none_of(printed_pages.begin(), printed_pages.end(), [current_rule](const page& printed_page){ return printed_page == current_rule.first; })) {
-                        // failed check: prerequisite has not been met!
-                        // skip to next update
-                        goto SKIP_TO_NEXT_UPDATE;
-                    }
-                }
-            }
-
-            printed_pages.push_back(current_page);
-
-        }
+        const bool update_is_valid = CheckUpdateValidity(current_update, relevant_rules);
 
         // successful update: write the metadata
-        metadata[i].is_valid = true;
-        center_idx = current_update.size()/2; // intentional truncation using integer division
-        metadata[i].center_page_value = current_update[center_idx];
+        if(update_is_valid) {
+            metadata[i].is_valid = true;
+            center_idx = current_update.size()/2; // intentional truncation using integer division
+            metadata[i].center_page_value = current_update[center_idx];
 
-        // update the sum
-        sum += metadata[i].center_page_value;
-
-        SKIP_TO_NEXT_UPDATE:;
+            // update the sum
+            sum += metadata[i].center_page_value;
+        }
 
     }
 
