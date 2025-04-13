4f178a7b93f2a1caed6f5818838f82193ff9705f
Augusto Dantas de Oliveira <guto@de-oliveira.org>
2025-04-12 18:13:28 -0700

    day 12: pulled out common region construction code in the sovler

--- day 12/include/solver.hh
+++ day 12/include/solver.hh
@@ -2,19 +2,45 @@
 #define _SOLVER_HH_
 
 #include "grid.hh"
+#include "region.hh"
 #include "types.hh"
 
 template<Parts p>
 class Solver {
 
   public:
-    Solver(const Grid<Plant>& parsedInput) :
-        data_(parsedInput) { }
+    Solver(const Grid<Plant>& parsedInput);
     unsigned int Solve();
 
   private:
     const Grid<Plant>& data_;
+    std::vector<Region> regions_;
 
 };
+
+template<Parts p>
+Solver<p>::Solver(const Grid<Plant>& parsedInput) :
+  data_(parsedInput) {
+
+    for(int x = 0; x < data_.MaxX(); x++) {
+        for(int y = 0; y < data_.MaxY(); y++) {
+            const Coordinate current_point = {x, y};
+
+            // check that this point is not part of a region already
+            bool visited = false;
+            for(const Region& region: regions_) {
+                visited = visited || region.PointIsInRegion(current_point);
+            }
+
+            // skip the following if this point is already accounted for
+            if(visited) continue;
+
+            Region region = Region(data_(current_point), data_, current_point);
+            regions_.push_back(std::move(region));
+
+        }
+    }
+
+}
 
 #endif
--- day 12/src/solver.cpp
+++ day 12/src/solver.cpp
@@ -8,30 +8,9 @@
 template<>
 unsigned int Solver<Parts::PART1>::Solve() {
 
-    std::vector<Region> regions;
-
-    for(int x = 0; x < data_.MaxX(); x++) {
-        for(int y = 0; y < data_.MaxY(); y++) {
-            const Coordinate current_point = {x, y};
-
-            // check that this point is not part of a region already
-            bool visited = false;
-            for(const Region& region: regions) {
-                visited = visited || region.PointIsInRegion(current_point);
-            }
-
-            // skip the following if this point is already accounted for
-            if(visited) continue;
-
-            Region region = Region(data_(current_point), data_, current_point);
-            regions.push_back(std::move(region));
-
-        }
-    }
-
     unsigned int price = 0;
 
-    for(const Region region : regions) {
+    for(const Region region : regions_) {
         price += (region.GetArea() * region.GetPerimeter());
     }
 
@@ -42,30 +21,9 @@ unsigned int Solver<Parts::PART1>::Solve() {
 template<>
 unsigned int Solver<Parts::PART2>::Solve() {
 
-    std::vector<Region> regions;
-
-    for(int x = 0; x < data_.MaxX(); x++) {
-        for(int y = 0; y < data_.MaxY(); y++) {
-            const Coordinate current_point = {x, y};
-
-            // check that this point is not part of a region already
-            bool visited = false;
-            for(const Region& region: regions) {
-                visited = visited || region.PointIsInRegion(current_point);
-            }
-
-            // skip the following if this point is already accounted for
-            if(visited) continue;
-
-            Region region = Region(data_(current_point), data_, current_point);
-            regions.push_back(std::move(region));
-
-        }
-    }
-
     unsigned int price = 0;
 
-    for(const Region region : regions) {
+    for(const Region region : regions_) {
         price += (region.GetArea() * region.GetSides());
     }
 
